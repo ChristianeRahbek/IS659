7132dfe73196cce78ab026aa7589509510e4cb3f
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2023-01-11 11:12:56 +0100

    changes to hstack

--- finestructure_fit/analysis1/hstack.cpp
+++ finestructure_fit/analysis1/hstack.cpp
@@ -9,28 +9,24 @@
 #include "TTree.h"
 
 void hstack() {
-    TFile *f=new TFile("/mnt/d/IS659/finestructure_fit/analysis1/output/bat/Run167mlio.root");
-    TTree *tr=(TTree*)f->Get("a");
-
-    auto hs = new THStack("hs","Stacked 1D histograms");
-    //create three 1-d histograms
-    //auto h1st = new TH1F("h1st","test hstack",100,-4,4);
+    auto *f1= new TFile("/mnt/d/IS659/finestructure_fit/analysis1/output/Run167mlio.root");
+    auto *tr1=(TTree*)f1->Get("a");
 
     auto h1st = new TH1F("h1st", "h1st", 1000, 0 , 8000);
-    tr -> Draw("Edssd >> h1st");
+    tr1 -> Draw("Edssd >> h1st");
     h1st ->SetLineColor(kRed);
-    h1st->SetMarkerStyle(21);
-    h1st->SetMarkerColor(kRed);
-    hs->Add(h1st);
+
+    auto *f2=new TFile("/mnt/d/IS659/finestructure_fit/analysis1/output/bat/Run167mlio.root");
+    auto *tr2=(TTree*)f2->Get("a");
 
     auto h2st = new TH1F("h2st", "h2st", 1000, 0 , 8000);
-    tr -> Draw("Edep0 >> h2st");
+    tr2 -> Draw("Edssd >> h2st");
     h2st ->SetLineColor(kBlue);
-    h2st->SetMarkerStyle(21);
-    h2st->SetMarkerColor(kBlue);
-    hs->Add(h2st);
 
-    auto cst = new TCanvas("cst","stacked hists",10,10,700,700);
+    auto hs = new THStack("hs","Stacked 1D histograms");
+    hs->SetHistogram(h1st);
+    hs->Add(h1st, "hist");
+    hs->Add(h2st, "hist");
 
     hs->Draw();
 

78beb7b0df190b7135075a8c33752092d8e96943
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2023-02-02 15:12:28 +0100

    further changes to angleAnalysis

--- finestructure_fit/analysis1/AngleAnalysis.cpp
+++ finestructure_fit/analysis1/AngleAnalysis.cpp
@@ -11,21 +11,19 @@
 #include <ausa/util/DynamicBranchVector.h>
 #include "TCanvas.h"
 #include "TH1F.h"
+#include <list>
 //#include "projectutil.h"
 
 using namespace std;
 using namespace AUSA;
 
 void AngleAnalysis() {
-    cout << "Opening document and making tree..." << endl;
     //string filePath = EUtil::getProjectRoot() + "output/Run167mlio.root";
     string filePath = "output/Run167mlio.root";
     string treeName = "a";
 
     auto *f= new TFile(filePath.c_str());
     auto *tr=(TTree*)f->Get(treeName.c_str());
-
-    cout << "Done, now initialising branches..." << endl;
 
     int det_a = 0;
     int det_b = 1;
@@ -38,8 +36,6 @@ void AngleAnalysis() {
     Short_t id[max_hits]; //list with length max_hits
     Double_t FT[max_hits];
 
-    cout << "Setting branch adresses..." << endl;
-
     //Referencing branches, so I can access in them in this code
     tr->SetBranchAddress("mul", &mul);
     tr->SetBranchAddress("TPROTONS", &TPROTONS);
@@ -49,22 +45,24 @@ void AngleAnalysis() {
 
     //looping over all entries
 
-    cout << "Making new tree" << endl;
     auto tr1 = new TTree("a1", "a1");
     //auto ang1 = make_unique<DynamicBranchVector<double>>(*tr1, "ang1", "mul");
-    cout << "Making new array" << endl;
-    cout << tr->GetEntries() << endl;
     //Double_t ang1[tr->GetEntries()];
-    Double_t ang1[max_hits];
+    list<double> ang1;
 
-    cout << "Done, now looping through entries" << endl;
+
+    cout << "Looping over entries" << endl;
     for(UInt_t i = 0; i < tr->GetEntries(); i++) {
+        cout << "Getting entry " + to_string(i) << endl;
         tr->GetEntry(i);
-
         //looping over every instance in an entry
+        cout << "Looping over instances" << endl;
         for(UInt_t j = 0; j < mul; j++){
+            cout << "Defining TVector3" << endl;
             TVector3 dir0, dir1;
+            cout << "Checking mul" << endl;
             if(mul < 2) break; //only do the following if we have at least 2 instances
+            cout << "Id checks" << endl;
             if(j == 0 && id[j] == 0){
                 dir0 = dir[j];
             }
@@ -73,12 +71,14 @@ void AngleAnalysis() {
             }
             else continue; //only looking at instance 0 in det 1 and instance 1 in det 2
 
+            cout << "Id check success, calculating angle" << endl;
             //calculating angle between the vectors
             auto ang = dir1.Angle(dir0);
 
+            cout << "Adding angle to list" << endl;
             //deciding what angles should be saved
             if (TPROTONS > 1000 && abs(FT[0]-FT[1])<2500) {
-                ang1[i] = ang;
+                ang1.emplace_back(ang);
             }
         }
     }
@@ -88,8 +88,8 @@ void AngleAnalysis() {
 
 
     auto hist = new TH1F("hist", "", 100, 0, 360);
-    for(int i = 0; i < tr->GetEntries(); i++){
-        hist->Fill(ang1[i]);
+    for(auto &a : ang1){
+        hist->Fill(a);
     }
     hist->Draw();
 

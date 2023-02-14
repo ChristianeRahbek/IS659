dfd0260fcf39ed188dda2a303c0437425a481b99
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2023-02-14 11:46:49 +0100

    Kolmogorov-Smirnof test backup

--- finestructure_fit/analysis1/KolmogorovTest.cpp
+++ finestructure_fit/analysis1/KolmogorovTest.cpp
@@ -6,48 +6,55 @@
 #include <iostream>
 #include <TH1F.h>
 #include <TTree.h>
+#include <TCanvas.h>
 
 using namespace std;
 
-Double_t KolmogorovTest(){
+void KolmogorovTest(){
     string filePath = "../../TPROTONS_id8_980.root";
     string myFilePath = "output/Run167mlio.root";
+
+    auto canv = new TCanvas();
+    canv->cd();
 
     auto *f= new TFile(filePath.c_str());
     auto *hist=(TH1F*)f->Get("h");
 
     Double_t* sample = new Double_t[hist->GetEntries()];
 
-    auto madsHist = new TH1F("madshist", "madshist", 1000,0,3500);
-    /*
+    auto madsHist = new TH1F("madshist", "madshist", 7000,0,3500);
+
     for(UInt_t i = 1; i <= hist->GetEntries(); i++) { //cut histogram to only the usable part
         if(hist->GetXaxis()->GetBinCenter(i) < 3500) {
             auto data = hist->GetBinContent(i);
-            sample[i-1] = data;
-            madsHist->Fill(data);
-        }
-    }
-     madsHist->Draw();
-     */
-    for(UInt_t i = 1; i <= hist->GetEntries(); i++) { //cut histogram to only the usable part
-        if(hist->GetXaxis()->GetBinCenter(i) >= 3500) {
-            hist->SetBinContent(i, 0);
+            //sample[i-1] = data;
+            madsHist->SetBinContent(i, hist->GetBinContent(i));
         }
     }
 
-    hist->Draw();
+    madsHist->Draw();
 
 
     auto *myfile = new TFile(myFilePath.c_str());
     auto *myTree=(TTree*)myfile->Get("a");
 
-    auto myHist = new TH1F("myhist", "myhist", 1000,0,3500);
+    auto myHist = new TH1F("myhist", "myhist", 7000,0,3500);
     myTree->Draw("TPROTONS >> myhist", "180/3.14159*acos((dir[0].X()*dir[1].X() + dir[0].Y()*dir[1].Y()+dir[0].Z()*dir[1].Z())/(pow(pow(dir[0].X(),2)+pow(dir[0].Y(),2)+pow(dir[0].Z(),2), 1/2)*pow(pow(dir[1].X(),2)+pow(dir[1].Y(),2)+pow(dir[1].Z(),2),1/2)))<=10 && ((Edep0[0]>0 && Edep0[1]>0) || (Edep1[0]>0 && Edep1[1]>0) || (Edep2[0]>0 && Edep2[1]>0) ||(Edep3[0]>0 && Edep3[1]>0)) && (Edssd[0]<800 || Edssd[1]<800) && abs(FT[1]-FT[0])<1500", "same");
+    auto pValLowAngle = madsHist->KolmogorovTest(myHist);
 
-    auto madsHistNormalized = hist->DrawNormalized();
-    auto myHistNormalized = myHist->DrawNormalized();
+    cout << "p-val for <=10 degrees: " << pValLowAngle << endl;
 
-    auto maxKolmogorovDistance = myHistNormalized->KolmogorovTest(madsHistNormalized, "N");
+    auto canvAlt = new TCanvas();
+    canvAlt->cd();
 
-    return maxKolmogorovDistance;
+    madsHist->Draw();
+
+    auto myHistAltnative = new TH1F("myhistAlt", "myhistAlt", 7000,0,3500);
+    myTree->Draw("TPROTONS >> myhistAlt", "180/3.14159*acos((dir[0].X()*dir[1].X() + dir[0].Y()*dir[1].Y()+dir[0].Z()*dir[1].Z())/(pow(pow(dir[0].X(),2)+pow(dir[0].Y(),2)+pow(dir[0].Z(),2), 1/2)*pow(pow(dir[1].X(),2)+pow(dir[1].Y(),2)+pow(dir[1].Z(),2),1/2)))>10 && ((Edep0[0]>0 && Edep0[1]>0) || (Edep1[0]>0 && Edep1[1]>0) || (Edep2[0]>0 && Edep2[1]>0) ||(Edep3[0]>0 && Edep3[1]>0)) && (Edssd[0]<800 || Edssd[1]<800) && abs(FT[1]-FT[0])<1500", "same");
+
+    auto pValBigAngle = madsHist->KolmogorovTest(myHistAltnative);
+
+
+    cout << "p-val for >10 degrees: " << pValBigAngle << endl;
+
 }

61ebf1474ca344119ba8338c1547095debd7ad64
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2023-02-22 14:05:38 +0100

    trying to get Plastic data

--- finestructure_fit/doSorting.sh
+++ finestructure_fit/doSorting.sh
@@ -1,10 +1,10 @@
-for run in 165 166; do
+for run in 167; do
     SETUP="../setup/setup.json"
 
     echo "Sorting run : $run using $SETUP"
     FILES="../runs/Run${run}*"
     echo "$FILES"
 
-    Sorter --ignore C1,C2,C3,C4,P1,P2,P3,P4 -s $SETUP -m ../sorting/matcher.json $FILES
+    Sorter --ignore C1,C2,C3,C4 -s $SETUP -m ../sorting/matcher.json $FILES
 done
 
